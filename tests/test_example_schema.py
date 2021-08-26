aa9cba3daa2f63797db598e47a56678b3ea4a583
Mahir-Sparkess <mahirrahman2000@gmail.com>
2021-08-26 10:57:25 +0100

    Error Report + Comments

--- tests/test_example_schema.py
+++ tests/test_example_schema.py
@@ -4,6 +4,24 @@ import yaml
 from cerberus import Validator
 
 from item_validator.schemas.base import base_schema
+from item_validator.schemas.extraction_methods import *
+from item_validator.schemas.pst_processors import *
+from item_validator.schemas.pre_processors import *
+
+subschema_map = {
+    'extraction_methods': {
+        'regex': regex_schema,
+        'header_extract': header_schema,
+        'iso19115': iso_schema,
+        'xml_extract': xml_schema,
+    },
+    'processors': {
+        'filename_reducer': filename_reducer_schema,
+        'ceda_observation': ceda_observation_schema,
+        'isodate_processor': isodate_schema,
+        'string_join': string_join_schema
+    }
+}
 
 
 class TextColours:
@@ -17,6 +35,35 @@ class TextColours:
     UNDERLINE = '\033[4m'
 
 
+def err_report(d: dict, v: Validator):
+    print(f"{TextColours.FAIL}Error Report\n{v.errors}")
+    if d.get('facets', {}).get('extraction_methods'):
+        extraction_methods = d['facets']['extraction_methods']
+        for method in extraction_methods:
+            if method.get('name') in subschema_map['extraction_methods'].keys():
+                schema = subschema_map['extraction_methods'].get(method['name'])
+                t = v.validate(method, schema['schema'])
+                if not t:
+                    print(f"Extraction Method: {method.get('name')} Failed!\n"
+                          f"{v.errors}")
+                    if method.get('pre_processors') or method.get('post_processors'):
+                        processors = method.get('pre_processors', []) + method.get('post_processors', [])
+                        for processor in processors:
+                            if processor.get('name') in subschema_map['processors'].keys():
+                                schema = subschema_map['processors'].get(processor['name'])
+                                t = v.validate(processor, schema['schema'])
+                                if not t:
+                                    print(f"Processor: {processor['name']} Failed!\n"
+                                          f"{v.errors}")
+                            else:
+                                print(f"No such processor {processor.get('name', 'or missing name')}")
+            else:
+                print(f"No such extraction method {method.get('name', 'or missing name')}")
+    else:
+        print(f"{v.errors}")
+    print(f"End Report{TextColours.ENDC}\n")
+
+
 def main():
     files = glob("descriptions/**/*.yml", recursive=True)
 
@@ -24,18 +71,17 @@ def main():
         with open(f, 'r') as stream:
             return yaml.safe_load(stream)
 
-    v = Validator(base_schema)
+    v = Validator()
 
     for f in files:
         d = load_dir(f)
-        t = v.validate(d)
+        t = v.validate(d, base_schema)
         print(f"Validating: {f.split('/')[-1]}..", end="")
         if t:
             print(f"{TextColours.BOLD}{TextColours.OKGREEN}{t}{TextColours.ENDC}")
         if not t:
-            err = v.errors
-            print(f"{TextColours.BOLD}{TextColours.FAIL}{t}{TextColours.ENDC}\n"
-                  f"{err}")
+            print(f"{TextColours.BOLD}{TextColours.FAIL}{t}{TextColours.ENDC}")
+            err_report(d, v)
 
 
 if __name__ == '__main__':
